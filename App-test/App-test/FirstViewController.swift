3539b47a98c011b54a62bc90e0eb689647762e5a
Yingjie Liang <yingjie@eduroam-int-dhcp-97-30-132.ucl.ac.uk>
2017-08-21 18:02:50 +0100

    add 2 views+storyboard

---
+++ App-test/App-test/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  App-test
+//
+//  Created by yingjie on 2017/8/21.
+//  Copyright © 2017年 yingjie. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
---
+++ App-test/App-test/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  App-test
+//
+//  Created by yingjie on 2017/8/21.
+//  Copyright © 2017年 yingjie. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+}
+
