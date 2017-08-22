fee6ed23027cc8644370c43e089dfe810ce1174f
Yingjie Liang <yingjie@eduroam-int-dhcp-98-63-120.ucl.ac.uk>
2017-08-22 14:57:00 +0100

    add interface

--- OCtest/OCtest/ViewController.m
+++ OCtest/OCtest/ViewController.m
@@ -9,15 +9,35 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
 @implementation ViewController
 
+
+- (IBAction)StartCam:(id)sender {
+    //    start + end the cam
+    //    didn't show until touch TakePic button
+}
+
+- (IBAction)PhotoLib:(id)sender {
+//import a pic
+//hide the cam button [subview setHidden:true];
+}
+- (IBAction)TakePic:(id)sender {
+//    use the pic
+//    show the cam button [subview setHidden:false];
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+
+
 
 
 - (void)didReceiveMemoryWarning {
