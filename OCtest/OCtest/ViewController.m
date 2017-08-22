3defd580c11de326fbd0a51738cf6501556b1fcd
Yingjie Liang <yingjie@eduroam-int-dhcp-98-63-120.ucl.ac.uk>
2017-08-22 16:37:12 +0100

    add pick from lib function

--- OCtest/OCtest/ViewController.m
+++ OCtest/OCtest/ViewController.m
@@ -8,20 +8,81 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+@property (strong, nonatomic) IBOutlet UIView *containerView;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (nonatomic) IBOutlet UIView *overlayView;
+
+@property (weak, nonatomic) IBOutlet UIButton *runStopBtn;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *fromLib;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *fromCam;
+@property (nonatomic) UIImagePickerController *imagePickerController;
+@property (nonatomic) NSMutableArray *capturedImages;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
 @implementation ViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.containerView sendSubviewToBack: _textView];
+    
+//    set up captured image array
+    self.capturedImages = [[NSMutableArray alloc] init];
+}
+
+- (void)showImage:(UIImagePickerControllerSourceType)sourceType fromButton:(UIBarButtonItem *)button
+{
+    if (self.imageView.isAnimating)
+    {
+        [self.imageView stopAnimating];
+    }
+    
+    if (self.capturedImages.count > 0)
+    {
+        [self.capturedImages removeAllObjects];
+    }
+    
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.modalPresentationStyle = UIModalPresentationCurrentContext;
+    imagePickerController.sourceType = sourceType;
+    imagePickerController.delegate = self;
+    imagePickerController.modalPresentationStyle =
+    (sourceType == UIImagePickerControllerSourceTypeCamera) ? UIModalPresentationFullScreen : UIModalPresentationPopover;
+    
+    UIPopoverPresentationController *presentationController = imagePickerController.popoverPresentationController;
+    presentationController.barButtonItem = button;  // display popover from the UIBarButtonItem as an anchor
+//    presentationController.permittedArrowDirections = UIPopoverArrowDirectionAny;
+    
+    if (sourceType == UIImagePickerControllerSourceTypeCamera)
+    {
+        // The user wants to use the camera interface. Set up our custom overlay view for the camera.
+        imagePickerController.showsCameraControls = NO;
+        
+        /*
+         Load the overlay view from the OverlayView nib file. Self is the File's Owner for the nib file, so the overlayView outlet is set to the main view in the nib. Pass that view to the image picker controller to use as its overlay view, and set self's reference to the view to nil.
+         */
+        [[NSBundle mainBundle] loadNibNamed:@"OverlayView" owner:self options:nil];
+        self.overlayView.frame = imagePickerController.cameraOverlayView.frame;
+        imagePickerController.cameraOverlayView = self.overlayView;
+        self.overlayView = nil;
+    }
+    
+    _imagePickerController = imagePickerController; // we need this for later
+    
+    [self presentViewController:self.imagePickerController animated:YES completion:^{
+        //.. done presenting
+    }];
+}
+
 - (IBAction)PhotoLib:(id)sender {
-//import a pic
-//hide the cam button [subview setHidden:true];
+    //hide the cam button [subview setHidden:true];
+    [self showImage:UIImagePickerControllerSourceTypePhotoLibrary fromButton:sender];
 }
 - (IBAction)TakePic:(id)sender {
-//    use the pic
-//    show the cam button [subview setHidden:false];
+    //    use the pic
+    //    show the cam button [subview setHidden:false];
 }
 
 - (IBAction)StartCamera:(id)sender {
@@ -29,18 +90,51 @@
     //    didn't show until touch TakePic button
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+// This method is called when an image has been chosen from the library or taken from the camera.
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    [self.capturedImages addObject:image];
+    
+    [self finishAndUpdate];
 }
 
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:^{
+        //.. done dismissing
+    }];
+    [self.textView setHidden:NO];
+    
+}
 
-
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)finishAndUpdate
+{
+    // Dismiss the image picker.
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    if ([self.capturedImages count] > 0)
+    {
+        if ([self.capturedImages count] == 1)
+        {
+            // Camera took a single picture.
+            [self.imageView setImage:[self.capturedImages objectAtIndex:0]];
+            [self.textView setHidden:YES];
+        }
+        else
+        {
+            // Camera took multiple pictures; use the list of images for animation.
+            self.imageView.animationImages = self.capturedImages;
+            self.imageView.animationDuration = 5.0;    // Show each captured photo for 5 seconds.
+            self.imageView.animationRepeatCount = 0;   // Animate forever (show all photos).
+            [self.imageView startAnimating];
+        }
+        
+        // To be ready to start again, clear the captured images array.
+        [self.capturedImages removeAllObjects];
+    }
+    
+    _imagePickerController = nil;
 }
 
 
