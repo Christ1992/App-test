e3286be8407caae51470c0b3c2d6416c5591106f
Yingjie Liang <yingjie@eduroam-int-dhcp-98-63-120.ucl.ac.uk>
2017-08-23 02:38:15 +0100

    add official code

--- OCtest/OCtest/ViewController.m
+++ OCtest/OCtest/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#include "tensorflow_utils.h"
+
 static void *AVCaptureStillImageIsCapturingStillImageContext =
 &AVCaptureStillImageIsCapturingStillImageContext;
 
@@ -115,28 +117,23 @@ static void *AVCaptureStillImageIsCapturingStillImageContext =
 {
     // Dismiss the image picker.
     [self dismissViewControllerAnimated:YES completion:nil];
+    [self.imageView setImage:[self.capturedImages objectAtIndex:0]];
     
-    if ([self.capturedImages count] > 0)
-    {
-        if ([self.capturedImages count] == 1)
-        {
-            // Camera took a single picture.
-            [self.imageView setImage:[self.capturedImages objectAtIndex:0]];
-            [self.textView setHidden:YES];
-        }
-        else
-        {
-            // Camera took multiple pictures; use the list of images for animation.
-            self.imageView.animationImages = self.capturedImages;
-            self.imageView.animationDuration = 5.0;    // Show each captured photo for 5 seconds.
-            self.imageView.animationRepeatCount = 0;   // Animate forever (show all photos).
-            [self.imageView startAnimating];
-        }
-        
-        // To be ready to start again, clear the captured images array.
-        [self.capturedImages removeAllObjects];
-    }
+    void * image_data = nil;
+    int width;
+    int height;
+    int channels;
     
+    std::vector<float> boxScore;
+    std::vector<float> boxRect;
+    std::vector<std::string> boxName;
+    
+    int ret = runModel(&image_data, &width, &height, &channels, boxScore, boxRect, boxName);
+    
+    
+    [self.textView setHidden:YES];
+    // To be ready to start again, clear the captured images array.
+    [self.capturedImages removeAllObjects];
     _imagePickerController = nil;
 }
 
@@ -299,6 +296,99 @@ static void *AVCaptureStillImageIsCapturingStillImageContext =
     }
 }
 
+- (void)captureOutput:(AVCaptureOutput *)captureOutput
+didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
+       fromConnection:(AVCaptureConnection *)connection {
+    CVPixelBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    CFRetain(pixelBuffer);
+    [self SetForModel:pixelBuffer];
+    CFRelease(pixelBuffer);
+}
+
+- (void)SetForModel:(CVPixelBufferRef)pixelBuffer {
+    assert(pixelBuffer != NULL);
+    OSType sourcePixelFormat = CVPixelBufferGetPixelFormatType(pixelBuffer);
+    int doReverseChannels;
+    if (kCVPixelFormatType_32ARGB == sourcePixelFormat) {
+        doReverseChannels = 1;
+    } else if (kCVPixelFormatType_32BGRA == sourcePixelFormat) {
+        doReverseChannels = 0;
+    } else {
+        assert(false);  // Unknown source format
+    }
+    
+    const int sourceRowBytes = (int)CVPixelBufferGetBytesPerRow(pixelBuffer);
+    const int image_width = (int)CVPixelBufferGetWidth(pixelBuffer);
+    const int fullHeight = (int)CVPixelBufferGetHeight(pixelBuffer);
+    
+    CVPixelBufferLockFlags unlockFlags = kNilOptions;
+    CVPixelBufferLockBaseAddress(pixelBuffer, unlockFlags);
+    
+    unsigned char *sourceBaseAddr =
+    (unsigned char *)(CVPixelBufferGetBaseAddress(pixelBuffer));
+    int image_height;
+    unsigned char *sourceStartAddr;
+    if (fullHeight <= image_width) {
+        image_height = fullHeight;
+        sourceStartAddr = sourceBaseAddr;
+    } else {
+        image_height = image_width;
+        const int marginY = ((fullHeight - image_width) / 2);
+        sourceStartAddr = (sourceBaseAddr + (marginY * sourceRowBytes));
+    }
+    const int image_channels = 4;
+    
+    assert(image_channels >= wanted_input_channels);
+    tensorflow::Tensor image_tensor(
+                                    tensorflow::DT_FLOAT,
+                                    tensorflow::TensorShape(
+                                                            {1, wanted_input_height, wanted_input_width, wanted_input_channels}));
+    auto image_tensor_mapped = image_tensor.tensor<float, 4>();
+    tensorflow::uint8 *in = sourceStartAddr;
+    float *out = image_tensor_mapped.data();
+    for (int y = 0; y < wanted_input_height; ++y) {
+        float *out_row = out + (y * wanted_input_width * wanted_input_channels);
+        for (int x = 0; x < wanted_input_width; ++x) {
+            const int in_x = (y * image_width) / wanted_input_width;
+            const int in_y = (x * image_height) / wanted_input_height;
+            tensorflow::uint8 *in_pixel =
+            in + (in_y * image_width * image_channels) + (in_x * image_channels);
+            float *out_pixel = out_row + (x * wanted_input_channels);
+            for (int c = 0; c < wanted_input_channels; ++c) {
+                out_pixel[c] = (in_pixel[c] - input_mean) / input_std;
+            }
+        }
+    }
+    
+    CVPixelBufferUnlockBaseAddress(pixelBuffer, unlockFlags);
+    
+    if (tf_session.get()) {
+        std::vector<tensorflow::Tensor> outputs;
+        tensorflow::Status run_status = tf_session->Run(
+                                                        {{input_layer_name, image_tensor}}, {output_layer_name}, {}, &outputs);
+        if (!run_status.ok()) {
+            LOG(ERROR) << "Running model failed:" << run_status;
+        } else {
+            tensorflow::Tensor *output = &outputs[0];
+            auto predictions = output->flat<float>();
+            
+            NSMutableDictionary *newValues = [NSMutableDictionary dictionary];
+            for (int index = 0; index < predictions.size(); index += 1) {
+                const float predictionValue = predictions(index);
+                if (predictionValue > 0.05f) {
+                    std::string label = labels[index % predictions.size()];
+                    NSString *labelObject = [NSString stringWithUTF8String:label.c_str()];
+                    NSNumber *valueObject = [NSNumber numberWithFloat:predictionValue];
+                    [newValues setObject:valueObject forKey:labelObject];
+                }
+            }
+            dispatch_async(dispatch_get_main_queue(), ^(void) {
+                [self setPredictionValues:newValues];
+            });
+        }
+    }
+    CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+}
 
 @end
 
